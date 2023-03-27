cf540e53053c30eec781ade727b02320fbeef624
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-27 14:43:24 -0600

    Added 2 inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react"
 
 function App() {
-  const [inputValue, setInputValue] = useState("3");
+  const [inputValue, setInputValue] = useState({
+    test1:"",
+    test2:""
+  });
   const handleChangeInput = (e) => {
-    setInputValue(e.target.value);
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value
+    });
+    console.log(inputValue);
   }
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Input Test</h1>
       <hr />
+      <label for = "test1" > test1</label>
       <input
-      onChange={(e) => handleChangeInput(e)}
-      type="text"
-      name="test"
-      value={inputValue}
+        id="test1"
+        onChange={(e) => handleChangeInput(e)}
+        type="text1"
+        name="test1"
+        value={inputValue.test1}
       />
+      <label for="test2" > test2
+      <input
+        className="mt-2"
+        onChange={(e) => handleChangeInput(e)}
+        type="text2"
+        name="test2"
+        value={inputValue.test2}
+        /></label>
     </div>
   )
 }
